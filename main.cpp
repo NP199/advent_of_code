fcc40f3c24b62407bfa7870095309f438e1f921b
niklas <niklas.pierick@stud.thga.de>
2023-12-05 14:44:34 +0100

    Solution 1

--- main.cpp
+++ main.cpp
@@ -2,81 +2,136 @@
 #include <exception>
 #include <fmt/core.h>
 #include <fstream>
-#include <regex>
-#include <set>
+#include <string>
+#include <vector>
+
+struct GameInfo {
+    unsigned int id;
+    unsigned int green;
+    unsigned int red;
+    unsigned int blue;
+};
+
+GameInfo parseLine(std::string& line) {
+    GameInfo          curGame{};
+    std::string const delimiters{":,;"};
+
+    while(!line.empty()) {
+        //find blob
+        std::size_t pos = line.find_first_of(delimiters);
+
+        //temp save blob
+        std::string substr{line.substr(0, pos)};
+        //fmt::print("{}\n", substr);
+
+        //parse blob
+        std::string name{};
+        std::string numStr{};
+        if(pos != std::string::npos) {
+            for(std::size_t i = 0; i < pos; ++i) {
+                if(!(std::isdigit(substr[i]))) {
+                    name += substr[i];
+                } else {
+                    numStr += substr[i];
+                }
+            }
+        } else {
+            for(std::size_t i = 0; i < line.size(); ++i) {
+                if(!(std::isdigit(substr[i]))) {
+                    name += substr[i];
+                } else {
+                    numStr += substr[i];
+                }
+            }
+        }
+
+        unsigned int number{std::stoi(numStr)};
+        name.erase(
+          remove_if(name.begin(), name.end(), [](unsigned char c) { return std::isspace(c); }),
+          name.end());
+        std::transform(name.begin(), name.end(), name.begin(), [](unsigned char c) {
+            return std::tolower(c);
+        });
+
+        if(name == "green") {
+            if(number <= 13) {
+                curGame.green += number;
+                fmt::print("possible\t{} {} {}\n", name, number, curGame.green);
+            }else{
+                fmt::print("not possible\t{} {} {}\n", name, number, curGame.green);
+                return GameInfo{};
+            }
+
+        } else if(name == "red") {
+            if(number <= 12) {
+                curGame.red += number;
+                fmt::print("possible\t{} {} {}\n", name, number, curGame.red);
+            }
+            else{
+                fmt::print("not possible\t{} {} {}\n", name, number, curGame.red);
+                return GameInfo{};
+            }
+        } else if(name == "blue") {
+            if(number <= 14) {
+                curGame.blue += number;
+                fmt::print("possible\t{} {} {}\n", name, number, curGame.blue);
+            }else{
+                fmt::print("not possible\t{} {} {}\n", name, number, curGame.blue);
+                return GameInfo{};
+            }
+        } else if(name == "game") {
+            curGame.id = number;
+            fmt::print("{} {} {}\n", name, number, curGame.id);
+        }
+
+        //truncate blob
+        if(pos != std::string::npos) {
+            line.erase(0, pos + 1);
+        } else {
+            line.erase(0, pos);
+        }
+    }
+    fmt::print("empty id parsed: {}\n", curGame.id);
+    return curGame;
+}
 
 int main() {
-    std::set<std::tuple<std::string, std::uint8_t>> strNums{
-      {  "one", 1},
-      {  "two", 2},
-      {"three", 3},
-      { "four", 4},
-      { "five", 5},
-      {  "six", 6},
-      {"seven", 7},
-      {"eight", 8},
-      { "nine", 9},
-      {    "0", 0},
-      {    "1", 1},
-      {    "2", 2},
-      {    "3", 3},
-      {    "4", 4},
-      {    "5", 5},
-      {    "6", 6},
-      {    "7", 7},
-      {    "8", 8},
-      {    "9", 9}
-    };
-
-    auto findFirstNum = [&](std::string const& str) {
-        std::size_t firstNum{str.size()};
-        std::uint8_t result{};
-        for(auto& strNum : strNums) {
-            std::size_t const num = str.find(std::get<0>(strNum));
-            if(firstNum > num) {
-                firstNum = num;
-                if(num != std::string::npos) {
-                    result = std::get<1>(strNum);
-                }
-            }
-        }
-        return result;
-    };
-    auto findLastNum = [&](std::string const& str) {
-        std::size_t lastNum{};
-        std::uint8_t result{};
-        for(auto& strNum : strNums) {
-            std::size_t const num = str.rfind(std::get<0>(strNum));
-            if(num != std::string::npos) {
-                if(lastNum <= num) {
-                    lastNum = num;
-                    result = std::get<1>(strNum);
-                }
-            }
-        }
-        return result;
-    };
-
-    std::string const filename{"../calibration.doc"};
+    std::string const filename{"../puzzle.txt"};
     std::fstream      fileHandle{filename, fileHandle.in};
     if(!fileHandle.is_open()) {
         fmt::print("Error opening file!\n");
         std::terminate();
     }
 
-    std::string   line;
-    std::uint64_t sumCalValues{0};
+    std::string           line;
+    unsigned int          sumIdValues{0};
+    std::vector<GameInfo> playedGames{};
     while(std::getline(fileHandle, line)) {
-        std::uint8_t tempFirstVal{findFirstNum(line)};
-        std::uint8_t tempLastVal{findLastNum(line)};
-        std::uint64_t calNumber{(tempFirstVal * 10) + tempLastVal};
-        sumCalValues += calNumber;
-        fmt::print(
-          "first: {}\t last: {}\t number:{}\t sum:{}\t",
-          tempFirstVal,
-          tempLastVal,
-          calNumber,
-          sumCalValues);
         fmt::print("{}\n", line);
+        playedGames.push_back(parseLine(line));
     }
+    fmt::print("");
+    GameInfo combinedGames{};
+    for(auto const& games : playedGames) {
+        if(games.red > 0  && games.green > 0 && games.blue > 0) {
+            fmt::print("possible\t");
+            fmt::print(
+              "id {}, red {} blue {} green {}\n",
+              games.id,
+              games.red,
+              games.blue,
+              games.green);
+
+            combinedGames.id += games.id;
+        } else {
+            fmt::print("not possible\t");
+            fmt::print(
+              "id {}, red {} blue {} green {}\n",
+              games.id,
+              games.red,
+              games.blue,
+              games.green);
+        }
+    }
+    fmt::print("result: {}\n", combinedGames.id);
 }
